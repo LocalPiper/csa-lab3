b69df22335e2d9abe17de91c60673de0a0b16af7
Local Piper <artsor53@gmail.com>
2024-06-22 02:36:22 +0300

    feat: added main to translator

--- translator.py
+++ translator.py
@@ -1,6 +1,19 @@
-def main():
-    pass
+def main(source, target):
+    with open(source, encoding="utf-8") as f:
+        source = f.read()
+
+    code, memory = translate_source(source)
+
+    write_to_json(target, code, memory)
+
+    print(
+        f"source LoC: {len(source.splitlines())} code instr: {len(code)}"
+    )
 
 
 if __name__ == "__main__":
-    pass
+    assert (
+        len(sys.argv) == 3
+    ), "Invalid usage: python translator.py <source_file> <target_file>"
+    _, source, target = sys.argv
+    main(source, target)
