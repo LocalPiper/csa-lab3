29c100c6ab11ef3dfc6d987c6b4929c0f6d12fad
Local Piper <artsor53@gmail.com>
2024-06-22 08:03:01 +0300

    fix: machine

--- machine.py
+++ machine.py
@@ -1,6 +1,18 @@
+#!/usr/bin/python3
+
+
+from __future__ import annotations
+
+import logging
+import sys
+
+from definitions import INPUT_ADDRESS, OUTPUT_ADDRESS, OUTPUT_NUM
+from isa import Command, Opcode, read_from_json
+
+
 class DataPath:
     data_memory_size: int
-    
+
     data_memory: list[int]
 
     data_address: int
@@ -11,18 +23,16 @@ class DataPath:
 
     data_stack: list[int]
 
-    address_register: int
 
     input_buffer: list
 
     output_buffer: list
 
-def __init__(self, data_memory_size, input_buffer):
+    def __init__(self, data_memory_size, input_buffer):
         assert data_memory_size > 0, "Data_memory size should be non-zero"
         self.data_memory_size = data_memory_size
         self.data_memory = [0] * data_memory_size
         self.data_address = 0
-        self.address_register = 0
         self.tos = 0
         self.buffer_register = 0
         self.data_address = 3
@@ -30,11 +40,8 @@ def __init__(self, data_memory_size, input_buffer):
         self.input_buffer = input_buffer
         self.output_buffer = []
 
-    def signal_latch_ar(self):
-        self.address_register = self.tos
-
     def signal_address(self):
-        self.data_address = self.address_register
+        self.data_address = self.tos
 
     def signal_latch_br(self, val):
         self.buffer_register = val
@@ -55,14 +62,6 @@ def __init__(self, data_memory_size, input_buffer):
     def signal_latch_tos(self, value):
         self.tos = value
 
-
-    def swap(self):
-        self.signal_latch_br(self.data_stack[-1])
-        self.data_stack.pop()
-        self.signal_data_push()
-        self.signal_bf_to_tos()
-
-
     def get_char(self):
         if self.data_address == INPUT_ADDRESS:
             if len(self.input_buffer) == 0:
@@ -78,26 +77,30 @@ def __init__(self, data_memory_size, input_buffer):
         return self.data_memory[self.data_address]
 
     def signal_wr(self):
+        logging.info("data_address: %s", self.data_address)
         if self.data_address  == OUTPUT_ADDRESS:
-            self.signal_data_pop()
-            if isinstance(self.tos, int):
-                symbol = chr(self.tos)
-            else:
-                symbol = self.tos
+            symbol = chr(self.tos)
             logging.debug(
                 "output: %s << %s", repr("".join(self.output_buffer)), repr(symbol)
             )
             self.output_buffer.append(symbol)
         elif self.data_address  == OUTPUT_NUM:
-            self.signal_data_pop()
             symbol = str(self.tos)
             logging.debug(
                 "output: %s << %s", repr("".join(self.output_buffer)), repr(symbol)
             )
             self.output_buffer.append(symbol)
         else:
-            self.data_memory[self.data_address] = self.signal_data_pop()
+            logging.info("Memory set: data_address: %s, tos: %s", self.data_address, self.tos)
+            self.data_memory[self.data_address] = self.tos
 
+
+    def swap(self):
+        self.signal_latch_br(self.tos)
+        self.signal_data_pop()
+        self.buffer_register, self.tos = self.tos, self.buffer_register
+        self.signal_data_push()
+        self.signal_latch_tos(self.buffer_register)
     def zero(self):
         return self.tos == 0
 
@@ -151,9 +154,17 @@ class ControlUnit:
     def signal_bf_to_tos(self):
         self.data_path.signal_latch_tos(self.data_path.buffer_register)
 
-    def decode_and_execute_control_flow_instruction(self, opcode):
-        self.data_path.swap()
+    def is_control_flow_instruction(self, opcode):
+        return opcode in [
+            Opcode.JMP,
+            Opcode.JZ,
+            Opcode.JNZ,
+        ]
+
+    def decode_and_execute_control_flow_instruction(self, opcode, address):
+        self.data_path.data_stack.append(address)
         if opcode is Opcode.JMP:
+            self.data_path.swap()
             self.program_counter = self.data_path.tos
             self.address_instr_mem = self.program_counter
             self.data_path.signal_data_pop()
@@ -161,37 +172,58 @@ class ControlUnit:
 
         if opcode is Opcode.JZ:
             if self.data_path.zero():
+                self.data_path.swap()
                 self.program_counter = self.data_path.tos
                 self.address_instr_mem = self.program_counter
-                self.data_path.swap()
+                self.data_path.signal_data_pop()
                 return True
-            self.data_path.data_stack.pop()
+            self.data_path.swap()
+            self.data_path.signal_data_pop()
+
+            return False
+
 
         if opcode is Opcode.JNZ:
             if not self.data_path.zero():
-                self.program_counter = self.data_path.tos
-                self.address_instr_mem = self.program_counter
                 self.data_path.swap()
-                return True
-            self.data_path.data_stack.pop()
-
-        if opcode is Opcode.JG:
-            if not self.data_path.negative() and not self.data_path.zero():
                 self.program_counter = self.data_path.tos
                 self.address_instr_mem = self.program_counter
                 self.data_path.signal_data_pop()
                 return True
+            self.data_path.swap()
+            self.data_path.signal_data_pop()
 
-        if opcode is Opcode.JGE:
-            if not self.data_path.negative() or self.data_path.zero():
+            return False
+
+        if opcode is Opcode.JL:
+            if self.data_path.negative() and not self.data_path.zero():
+                self.data_path.swap()
                 self.program_counter = self.data_path.tos
                 self.address_instr_mem = self.program_counter
                 self.data_path.signal_data_pop()
                 return True
-            self.data_path.data_stack.pop()
+            self.data_path.swap()
+            self.data_path.signal_data_pop()
+
+            return False
+
+
+        if opcode is Opcode.JLE:
+            if self.data_path.negative() or self.data_path.zero():
+                self.data_path.swap()
+                self.program_counter = self.data_path.tos
+                self.address_instr_mem = self.program_counter
+                self.data_path.signal_data_pop()
+                return True
+            self.data_path.swap()
+            self.data_path.signal_data_pop()
+
+            return False
+
 
 
         if opcode is Opcode.CALL:
+            self.data_path.swap()
             self.signal_ret_push()
             self.program_counter = self.data_path.tos
             self.address_instr_mem = self.data_path.tos
@@ -203,18 +235,21 @@ class ControlUnit:
             self.address_command_reg = self.program_counter
             self.address_instr_mem = self.program_counter
             return True
+        self.data_path.data_stack.pop()
         return False
 
     def decode_and_execute_instruction(self):
         instr = self.instruction_memory[self.program_counter]
         opcode = instr.opcode
 
-        if instr.operand is not None:
-            self.data_path.data_stack.append(self.data_path.tos)
-            self.data_path.tos = int(instr.operand)
 
-        if self.decode_and_execute_control_flow_instruction(opcode):
-            return
+        if instr.operand is not None:
+            if self.decode_and_execute_control_flow_instruction(opcode, int(instr.operand)):
+                return
+        if not self.is_control_flow_instruction(opcode):
+                    logging.info("Instruction has operand: %s", instr.operand)
+                    self.data_path.signal_data_push()
+                    self.data_path.signal_latch_tos(int(instr.operand))
 
         if opcode is Opcode.HALT:
             raise StopIteration()
@@ -229,15 +264,16 @@ class ControlUnit:
             self.data_path.signal_data_pop()
 
         if opcode == Opcode.ST:
-            self.data_path.signal_latch_ar()
             self.data_path.signal_address()
+            self.data_path.signal_data_pop()
+            logging.info("We are executing ST, TOS is %s", self.data_path.tos)
             self.data_path.signal_wr()
 
 
         if opcode == Opcode.LD:
-            self.data_path.signal_latch_ar()
             self.data_path.signal_address()
             self.data_path.signal_mem_to_tos()
+
 
         if opcode == Opcode.INC:
             self.data_path.signal_latch_tos(
@@ -267,9 +303,19 @@ class ControlUnit:
             )
             self.data_path.data_stack.pop()
         if opcode == Opcode.SWAP:
-            self.data_path.swap()
+            self.data_path.signal_latch_br(self.data_path.data_stack[-1])
+            self.data_path.data_stack.pop()
+            self.data_path.signal_data_push()
+            self.data_path.signal_bf_to_tos()
+
         if opcode == Opcode.DUP:
             self.data_path.signal_data_push()
+
+        if opcode is Opcode.RET:
+            self.signal_ret_pop()
+            self.address_command_reg = self.program_counter
+            self.address_instr_mem = self.program_counter
+            return
 
         self.signal_latch_program_counter(sel_next=True)
 
@@ -289,7 +335,8 @@ class ControlUnit:
         if instr.operand is not None:
             instr_repr += " {}".format(instr.operand)
 
-        return "{} \t{}".format(state_repr, instr_repr) 
+        return "{} \t{}".format(state_repr, instr_repr)
+
 
 def simulation(code, memory, input_tokens, data_memory_size, limit):
     data_path = DataPath(data_memory_size, input_tokens)
@@ -334,12 +381,11 @@ def main(code_file, input_file):
         memory,
         input_tokens=input_token,
         data_memory_size=300,
-        limit=30,
+        limit=1000
     )
 
     print("".join(output))
     print("instr_counter: ", instr_counter)
-
 
 
 if __name__ == "__main__":
@@ -347,3 +393,4 @@ if __name__ == "__main__":
     assert len(sys.argv) == 3, "Wrong arguments: machine.py <code_file> <input_file>"
     _, code_file, input_file = sys.argv
     main(code_file, input_file)
+
